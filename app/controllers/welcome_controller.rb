7af2029c494afd53c929e6e41ae2483e3bd19147
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-24 23:48:41 -0700

    made signed_in home page to work

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,15 @@
 class WelcomeController < ApplicationController
 
   def home
+    binding.pry
+    redirect_to personal_home_path if user_signed_in?
   end
 
   def logged_in_home
-    binding.pry
-    if !user_signed_in?
-      redirect_to root_path
-    end
+    # binding.pry
+    # if !user_signed_in?
+      # redirect_to root_path
+    # end
   end
 
 end
