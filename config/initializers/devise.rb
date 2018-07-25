0865da43f12113d40db499d0542e76936b11449d
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-24 21:40:43 -0700

    devise custom views folder is a go

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -9,7 +9,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = '919128b5e2764c1242618c694bf78a39f0d2ddaa5ae78b3d612e2c29562147e4a2c2a07b153897fef385bbdb1252d06e4d3ad4ecaa74e98a7d167bd6f077fc63'
-  
+
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -229,7 +229,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
