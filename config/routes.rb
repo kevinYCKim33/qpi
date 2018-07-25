167927bff3b5fda89c3a8e4d88cb16b59af1ba5e
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-24 21:17:20 -0700

    devise logged in as george

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'welcome#home'
   get 'css/normalize.css', to: 'css#normalize'
   get 'css/webflow.css', to: 'css#webflow'
   get 'css/qpi.css', to: 'css#qpi'
