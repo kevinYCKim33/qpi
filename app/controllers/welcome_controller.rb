ee9c699430291822d9adf8affba2fa306df0395f
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-24 23:59:22 -0700

    log in log out current user all working in beta mode

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,15 +1,15 @@
 class WelcomeController < ApplicationController
 
   def home
-    binding.pry
+    # binding.pry
     redirect_to personal_home_path if user_signed_in?
   end
 
   def logged_in_home
     # binding.pry
-    # if !user_signed_in?
-      # redirect_to root_path
-    # end
+    if !user_signed_in?
+      redirect_to root_path
+    end
   end
 
 end
