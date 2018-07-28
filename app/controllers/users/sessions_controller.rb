4a82232271f0b13d801f4ac546e8c8f50f062f59
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-28 01:18:45 -0700

    header and footer all set for non user pages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,9 +4,11 @@ class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    # @title = 'Log in'
+    # render :layout => 'landing'
+    super
+  end
 
   # POST /resource/sign_in
   # def create
