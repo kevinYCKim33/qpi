f65e95717b0a63a5462e2e7ecba0e2843503d0f3
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-07-31 02:39:56 -0700

    landing page image all fixed, potential waste of time

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,11 +1,14 @@
 class ImagesController < ApplicationController
 
-  def Manzanita
-    send_file 'public/Manzanita.jpeg', type: 'image/jpeg'
-  end
-
-  def handshake_unsplash
-    send_file 'public/handshake_unsplash.jpg', type: 'image/jpg'
+  # def Manzanita
+  #   send_file 'public/Manzanita.jpeg', type: 'image/jpeg'
+  # end
+  #
+  # def handshake_unsplash
+  #   send_file 'public/handshake_unsplash.jpg', type: 'image/jpg'
+  # end
+  def KidandCar
+    send_file 'app/assets/images/KidandCar.png', type: 'image/png'
   end
 
 end
