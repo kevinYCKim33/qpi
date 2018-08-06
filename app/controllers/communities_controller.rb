b46f97b8078b3b4fbbe4ba311e7a21faecf5dcb7
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-08-06 06:18:40 -0700

    cute flash message for url hacking into unjoined community

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -3,8 +3,16 @@ class CommunitiesController < ApplicationController
     @communities = Community.all
   end
 
+  def my_communities
+    @communities = current_user.communities
+  end
+
   def show
     @community = Community.find(params[:id])
     @community_user = CommunityUser.find_by(user_id: current_user.id, community_id: @community.id)
+    if @community_user == nil
+      flash[:message] = "You have not joined that community yet!"
+      redirect_to communities_path
+    end
   end
 end
