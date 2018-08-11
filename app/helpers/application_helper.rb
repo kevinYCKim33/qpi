c6477a0b75885846fafcafb5f16dfe556c823d1c
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-08-11 02:59:26 -0700

    platform reactv2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@ module ApplicationHelper
   def active?(path)
     'w--current' if current_page?(path)
   end
+
+  # will break if user inputs something like twitter.com/httpsIsCool
+  # i'm willing to take that chance
+  def append_https_if_necessary(user_input_link)
+    if user_input_link.include?("https")
+      link = user_input_link
+    else
+      link = "https://" + user_input_link
+    end
+    return link
+  end
 end
