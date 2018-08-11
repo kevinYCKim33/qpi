f26f007b2b42d06d12ca870a43b505a51803c042
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-08-11 03:47:27 -0700

    user profile done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,8 @@ module ApplicationHelper
     'w--current' if current_page?(path)
   end
 
-  # will break if user inputs something like twitter.com/httpsIsCool
-  # i'm willing to take that chance
   def append_https_if_necessary(user_input_link)
-    if user_input_link.include?("https")
+    if user_input_link.index('https://') == 0
       link = user_input_link
     else
       link = "https://" + user_input_link
