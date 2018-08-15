6101b5db0e854a189efa9d8ac179cc440010e6a9
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-08-15 02:28:00 -0700

    active admin works for workshops

--- app/controllers/workshops_controller.rb
+++ app/controllers/workshops_controller.rb
@@ -3,4 +3,11 @@ class WorkshopsController < ApplicationController
     @title = "Workshops"
     @workshops = Workshop.all
   end
+
+  private
+
+  # def workshop_params
+  #   # binding.pry
+  #   params.require(:workshop).permit(:title, :location, :link, :date, :description, :community_id)
+  # end
 end
