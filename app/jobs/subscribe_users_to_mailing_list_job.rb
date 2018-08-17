b6a635d6ded521e0b3a3b2c66378ab9a9ebd0cc8
Kevin Yongchul Kim <kevikim33@gmail.com>
2018-08-17 07:17:29 -0700

    bunch of files cleaned up

--- app/jobs/subscribe_users_to_mailing_list_job.rb
+++ app/jobs/subscribe_users_to_mailing_list_job.rb
@@ -1,20 +1,22 @@
-class SubscribeUsersToMailingListJob < ApplicationJob
-  queue_as :default
+# none of these used right now, but probably good idea to have a background worker handle email logic
 
-  def perform(*args)
-    # Do something later
-  end
-end
-
-
-class SubscribeUserToMailingListJob < ActiveJob::Base
-  queue_as :default
-
-  def perform(user)
-    gibbon = Gibbon::Request.new
-    # gibbon.lists(list_id)({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => user.email}, :double_optin => false})
-    gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {FULLNAME: user.name}})
-  end
-end
+# class SubscribeUsersToMailingListJob < ApplicationJob
+#   queue_as :default
+#
+#   def perform(*args)
+#     # Do something later
+#   end
+# end
+#
+#
+# class SubscribeUserToMailingListJob < ActiveJob::Base
+#   queue_as :default
+#
+#   def perform(user)
+#     gibbon = Gibbon::Request.new
+#     # gibbon.lists(list_id)({:id => ENV["MAILCHIMP_LIST_ID"], :email => {:email => user.email}, :double_optin => false})
+#     gibbon.lists(ENV["MAILCHIMP_LIST_ID"]).members.create(body: {email_address: user.email, status: "subscribed", merge_fields: {FULLNAME: user.name}})
+#   end
+# end
 
 # gibbon.lists(list_id).members.create(body: {email_address: "foo@bar.com", status: "subscribed", merge_fields: {FNAME: "First Name", LNAME: "Last Name"}})
